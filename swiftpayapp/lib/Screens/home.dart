d41a24c5ec4c2e0a4ba7071ecd04820dc091fc00
richard kasomi <167867300+str4wh@users.noreply.github.com>
2024-10-01 17:16:59 +0300

    comment

--- swiftpayapp/lib/Screens/home.dart
+++ swiftpayapp/lib/Screens/home.dart
@@ -164,7 +164,7 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         Text(
-                          'Enjelin Morgeana',
+                          'RICHARD KASOMI',
                           style: TextStyle(
                             fontWeight: FontWeight.w600,
                             fontSize: 20,
